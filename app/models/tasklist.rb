a3a899bdab49d1d7e192b8cbf125cd5800685bbe
Roberto Hernandez <carlosh95@gmail.com>
2014-12-15 22:31:31 -0500

    cleanup from tasks and lists model

--- app/models/tasklist.rb
+++
@@ -1,6 +0,0 @@
-class Tasklist < ActiveRecord::Base
-	has_many :tasks, dependent: :destroy
-    validates :title, presence: true
-    validates :description, presence: true
-    validates :due_date,presence: true
-end
